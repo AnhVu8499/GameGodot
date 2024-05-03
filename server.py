8024700dee14230c08c5b4678a76218b09170efe
AnhVu8499 <98485978+AnhVu8499@users.noreply.github.com>
2024-05-03 12:09:10 -0700

    Update server.py\

--- server.py
+++ server.py
@@ -9,10 +9,12 @@ class HTTPServer(server.SimpleHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+        self.send_header("Cross-Origin-Resource-Policy", "cross-origin")
+        self.send_header("Cross-Origin-Embedder-Policy-Report-Only", "unsafe-none")
+        self.send_header("Cross-Origin-Opener-Policy-Report-Only", "unsafe-none")
 
 if __name__ == '__main__':
     server_address = ('', 8080)  # Listen on all available network interfaces
     httpd = server.HTTPServer(server_address, HTTPServer)
     print("Server running on port 8080")
     httpd.serve_forever()
-
